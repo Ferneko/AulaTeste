198a3478478c930a8969595451d102e15d6d61f2
Ferneko <fcgraciano@gmail.com>
2022-08-16 12:31:28 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeira Aula</title>
+    <style>
+        body {
+            background-color: red;
+        }
+        
+    </style>
+</head>
+
+<body>
+
+    <?php
+    if (10 > 12) {
+        echo "Função Echo nativa";
+        echo "<h2> Meu subtítulo dinâmico </h2> ";
+    } else {
+        echo "Condição falsa do PHP";
+    }
+
+
+    ?>
+
+    <h1>Fernando Graciano</h1>
+    <p>
+        Hello World!
+    </p>
+    <br />
+    Nossa primeira aula de php básico
+
+    <script>
+        if (10 > 12) {
+
+        } else {
+            console.log("Condição falsa javascript")
+        }
+    </script>
+</body>
+
+</html>
